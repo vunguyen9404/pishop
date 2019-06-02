af4d4a435b64bb245991f75b581605822b38660e
Nguyễn Vũ <vunvd00172@fpt.edu.vn>
2019-06-03 03:58:07 +0700

    Add Sass build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 // next.config.js
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require('@zeit/next-sass');
-module.exports = withTypescript(withSass());
+module.exports = withTypescript(withSass(
+    {
+        cssModules: true,
+        cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: "_[hash:base64:9]",
+        }
+    }
+));
